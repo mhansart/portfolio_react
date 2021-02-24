a2a3b0c92f27582bd29bcb2e9f6e44b34f1a3b00
Marine Hansart <hansart.marine@gmail.com>
2021-02-24 22:19:40 +0100

    first

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b1842fbc6bb759993684ed86aabfda1b9c4bab9e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b1842fbc6bb759993684ed86aabfda1b9c4bab9e',
+    ),
+  ),
+);
