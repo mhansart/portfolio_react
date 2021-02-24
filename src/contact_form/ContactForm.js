e6abb35407c31c7a083ca819586b929ae8764a03
Marine Hansart <hansart.marine@gmail.com>
2021-02-24 21:25:10 +0100

    add ajax request

--- src/contact_form/ContactForm.js
+++ src/contact_form/ContactForm.js
@@ -26,7 +26,7 @@ function ContactForm() {
       data.append('email', email);
       data.append('name', name);
       data.append('message', description);
-      ajaxPost('mail.php?task=write', data)
+      ajaxPost('http://localhost/mail-service/client/public/mail.php?task=write', data)
         .then((response) => {
           console.log(response);
           if (response === 'success') {
